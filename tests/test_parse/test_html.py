881de13be0efc58f6bc3ff82c0b84d841db878dd
trducng <duc@chunking.com>
2025-04-25 14:55:32 +0700

    parse/html: preliminary html parsing using pandoc

---
+++ tests/test_parse/test_html.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from chunking.base import Chunk
+from chunking.mime import mime_html
+from chunking.parse.html import PandocHtmlParser
+
+html_path = str(Path(__file__).parent.parent / "assets" / "long.html")
+
+
+def test_pandoc_html():
+    root = mime_html.as_root_chunk(html_path)
+    chunks = PandocHtmlParser.run(root)
+    chunk = chunks[0]
+    assert isinstance(chunk, Chunk)
