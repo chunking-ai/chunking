0f24145e6f780741ec98df99f3351caebce325a2
trducng <duc@chunking.com>
2025-05-01 00:40:40 +0700

    split/propositionizer: add propositionizer approach (from dense-x-retrieval)

---
+++ tests/test_split/test_propositionizer.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from chunking.base import Chunk
+from chunking.mime import mime_docx
+from chunking.parse.pandoc_engine import PandocEngine
+from chunking.split.propositionizer import Propositionizer
+
+docx_path = str(Path(__file__).parent.parent / "assets" / "with_image.docx")
+
+
+def test_propositionizer():
+    root = mime_docx.as_root_chunk(docx_path)
+    chunks = PandocEngine.run(root)
+    chunk = chunks[0]
+
+    temp = chunk.child.next.next.next.next.next.next.next
+    temp.next = None
+    output = Propositionizer.run(temp)
+    assert isinstance(output[0], Chunk)
