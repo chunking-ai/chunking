cf5646eae7d402bbf73a1e5293dacb593f9bb314
trducng <duc@chunking.com>
2025-05-07 22:29:42 +0700

    refactor: rename module `router` to `controller`

--- tests/test_parse/test_directory.py
+++ tests/test_parse/test_directory.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 
 from chunking.base import Chunk
+from chunking.controller import Controller
 from chunking.parser.directory import DirectoryParser
-from chunking.router import FileCoordinator
 
 path = str(Path(__file__).parent.parent)
-_file = FileCoordinator()
+ctrl = Controller()
 
 
 def test_directory():
-    root = _file.as_root_chunk(path)
+    root = ctrl.as_root_chunk(path)
     chunks = DirectoryParser.run(root)
     chunk = chunks[0]
     assert isinstance(chunk, Chunk)
--- tests/test_parse/test_text.py
+++ tests/test_parse/test_text.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 
 from chunking.base import Chunk
+from chunking.controller import Controller
 from chunking.parser.text import TextParser
-from chunking.router import FileCoordinator
 
 file_path = str(Path(__file__).parent.parent / "assets" / "long.txt")
-_file = FileCoordinator()
+ctrl = Controller()
 
 
 def test_text():
-    root = _file.as_root_chunk(file_path)
+    root = ctrl.as_root_chunk(file_path)
     chunks = TextParser.run(root)
     chunk = chunks[0]
     assert isinstance(chunk, Chunk)
