b02191bc28f55a1e28bcf27ebb8b6b3d36e17fe7
trducng <duc@chunking.com>
2025-05-07 15:20:11 +0700

    parse/directory: add vanilla directory parser

---
+++ tests/test_parse/test_directory.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from chunking.base import Chunk
+from chunking.parse.directory import DirectoryParser
+from chunking.router import FileCoordinator
+
+path = str(Path(__file__).parent.parent)
+_file = FileCoordinator()
+
+
+def test_directory():
+    root = _file.as_root_chunk(path)
+    chunks = DirectoryParser.run(root)
+    chunk = chunks[0]
+    assert isinstance(chunk, Chunk)
