9efd59c14cd2739a310ad98f545885b7415e8679
trducng <duc@chunking.com>
2025-05-07 09:11:54 +0700

    router: preliminary router

--- tests/test_parse/test_pptx.py
+++ tests/test_parse/test_pptx.py
@@ -1,22 +1,23 @@
 from pathlib import Path
 
 from chunking.base import Chunk
-from chunking.mime import mime_pptx
 from chunking.parse.pptx import PptxParser
+from chunking.router import FileCoordinator
 
 pptx_path = str(Path(__file__).parent.parent / "assets" / "normal.pptx")
 pptx_short = str(Path(__file__).parent.parent / "assets" / "short_image.pptx")
+_file = FileCoordinator()
 
 
 def test_pptx_fast():
-    root = mime_pptx.as_root_chunk(pptx_path)
+    root = _file.as_root_chunk(pptx_path)
     chunks = PptxParser.run(root)
     chunk = chunks[0]
     assert isinstance(chunk, Chunk)
 
 
 def test_pptx_image():
-    root = mime_pptx.as_root_chunk(pptx_short)
+    root = _file.as_root_chunk(pptx_short)
     chunks = PptxParser.run(root, caption=True)
     chunk = chunks[0]
     assert isinstance(chunk, Chunk)
